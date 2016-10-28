f79a14bd544288b78f81910241855022b67899d8
Jan Phillip Carreon <janphillipcarreon@yahoo.com>
2016-10-27 16:21:20 -1000

    initialize redux

--- entry.js
+++ entry.js
@@ -1,10 +1,17 @@
-'use strict';
+ 'use strict';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import KanBanPage from './components/KanBanPage';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import * as reducers from './reducers';
 
+const reducer = combineReducers(reducers);
+const store = createStore(reducer);
 
 ReactDOM.render(
-  <KanBanPage url='http://localhost:3000/api' />,
+  <Provider store={ store } >
+    <KanBanPage url='http://localhost:3000/api' />
+  </Provider>,
   document.getElementById('root')
 );
