6c4f9484498e2bcced6e0c256f15ae7ad6f9ce96
Renee <rchar20@gmail.com>
2016-11-26 20:26:40 -1000

    removes publicPath modification in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: '[name].js',
-    publicPath: '/React-Kanban',
+    publicPath: '/',
   },
   resolve: {
     extensions: ['', '.js'],
